7a43496a8ac0649c302acfe03e84451c7789f71d
山岸 大 <m-yamagishi@infiniteloop.co.jp>
2019-06-05 17:32:56 +0900

    chore: Delete dist files

--- dist/test/index.d.ts
+++
@@ -1,4 +0,0 @@
-import '@babylonjs/core/Helpers/sceneHelpers';
-import '@babylonjs/core/Meshes/Builders/sphereBuilder';
-import '@babylonjs/core/Meshes/Builders/torusKnotBuilder';
-import '@babylonjs/inspector';
