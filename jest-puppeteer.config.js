d78e303b4e5b921d255328acea297471325b5a0c
horyu <28817604+horyu@users.noreply.github.com>
2022-04-27 11:11:54 +0900

    test(jest-puppeteer.config.js): fix port setting

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,14 @@
 module.exports = {
     server: {
         command: 'yarn http-server ./test -p 10080',
+        port: 10080,
     },
     launch: {
         // headless: false,
         // devtools: true,
+        args: [
+            '--explicitly-allowed-ports=10080',
+        ],
     },
     browserContext: 'default',
 };
